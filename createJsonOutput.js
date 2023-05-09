7dc084615b919affdf15b93444cd139dac2fe291
emiyl <me@emiyl.com>
2023-05-09 12:15:49 +0100

    create json output of images in repo

---
+++ createJsonOutput.js
@@ -0,0 +1,71 @@
+const fs = require('fs')
+const { get } = require('http')
+
+function getDirContents(p) {
+    return fs.readdirSync(p, function (err, files) {
+        var retArr = []
+        files.forEach(f => retArr.push(f))
+        return retArr
+    })
+}
+
+const keyArr = new Set(...[
+        [
+        ...getDirContents('images'),
+        ...getDirContents('images-lowres')
+    ].filter(x => x != '.DS_Store')
+    .map(x => x.includes('.') ? x.split('.').slice(0,-1).join() : x) // Remove file extension
+])
+
+let retArr = []
+
+for (const key of keyArr) {
+    let lowresExists = false
+    let pngExists = false
+    let folderExists = false
+
+    if (fs.existsSync(`images-lowres/${key}.png`))  lowresExists    = true
+    if (fs.existsSync(`images/${key}.png`))         pngExists       = true
+    if (fs.existsSync(`images/${key}/0.png`))       folderExists    = true
+    
+    if (folderExists) {
+        const folderContents = getDirContents(`images/${key}`)
+        let images = []
+
+        for (let i = 0;; i++) {
+            if (folderContents.includes(`${i}.png`)) images.push({
+                id: i,
+                dark: folderContents.includes(`${i}_dark.png`)
+            })
+            else break
+        }
+
+        retArr.push({
+            key: key,
+            count: images.length,
+            dark: images.map(x => x.dark).filter(x => !x).length,
+            index: images
+        })
+
+        continue
+    }
+
+    if (pngExists || lowresExists) {
+        retArr.push({
+            key: key,
+            count: 1,
+            dark: false,
+            index: [{
+                id: 0,
+                dark: false
+            }]
+        })
+
+        continue
+    }
+}
+
+if (!fs.existsSync('out')) fs.mkdirSync('out')
+fs.writeFile('out/main.json', JSON.stringify(retArr), (err) => {
+    if (err) console.log(err)
+})
