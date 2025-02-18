a310cae7baab9fc8f5dbfbeea67f8bd40a73a272
Ryan Seaboyer <ryan.seaboyer@icloud.com>
2025-02-17 21:37:46 -0400

    Reapply improved performance change

--- index.js
+++ index.js
@@ -1,46 +1,36 @@
-const path = require('path')
-const fs = require('fs')
-const sharp = require("sharp")
-const { create } = require('domain')
+import { resolve, join, basename } from 'path'
+import { readdirSync, statSync, mkdirSync } from 'fs'
+import sharp from "sharp"
 
 const resizeArr = [32,64,128,256,512,1024,'main','preview']
 
-const imgPath = path.resolve(__dirname, 'out')
-const deviceDirPath = path.resolve(__dirname, "device")
-const lowResDirPath = path.resolve(__dirname, "device-lowres")
-const imageDirPath = path.resolve(__dirname, "images")
+const __dirname = import.meta.dirname
+
+const imgPath = resolve(__dirname, 'out')
+const deviceDirPath = resolve(__dirname, "device")
+const lowResDirPath = resolve(__dirname, "device-lowres")
+const imageDirPath = resolve(__dirname, "images")
 
 const argDevices = process.argv.slice(2)
 
-function getDirContents(p) {
-  return fs.readdirSync(p, function (err, files) {
-    var retArr = []
-    files.forEach(f => retArr.push(f))
-    return retArr
-  })
-}
-
 function getPngs(p, dirPath) {
-  let key = undefined
-  const dirArr = getDirContents(p).map(x => {
-    const recursePath = path.join(dirPath, x)
+  return readdirSync(p).map(x => {
+    const recursePath = join(dirPath, x)
     
     if (x.endsWith('.png')) return {
-      key: path.basename(x,'.png'),
+      key: basename(x,'.png'),
       imageArr: false
     }
-    else if (fs.statSync(recursePath).isDirectory()) return {
+    else if (statSync(recursePath).isDirectory()) return {
       key: x,
-      imageArr: getDirContents(recursePath).filter(x => x.endsWith('.png'))
+      imageArr: readdirSync(recursePath).filter(x => x.endsWith('.png'))
     }
-  }).filter(x => x).filter(x => {
+  }).filter(x => {
+    if (!x) return false
     if (argDevices.length) return argDevices.includes(x.key)
     return true
   })
-  return dirArr
 }
-
-function mkDir(p) { if (!fs.existsSync(p)) fs.mkdirSync(p) }
 
 const deviceDirArr = getPngs(deviceDirPath, deviceDirPath)
 const lowResDirArr = getPngs(lowResDirPath, deviceDirPath).filter(x => !deviceDirArr.map(y => y.key).includes(x.key))
@@ -48,15 +38,6 @@ const imageDirArr = getPngs(imageDirPath, imageDirPath)
 
 async function createImg(img, res, dir, outputFormat, outputDir) {
   try {
-    const outDirArr = [imgPath, `${outputDir}@${res}`, img.key]
-    for (const o in outDirArr) mkDir(
-      path.join(...
-        outDirArr.filter((x, index) => {
-          return index <= o
-        })
-      )
-    )
-
     let options = {
       width: res,
       height: res,
@@ -66,41 +47,38 @@ async function createImg(img, res, dir, outputFormat, outputDir) {
     if (res == 'preview') options = { height: 256 }
     else if (res == 'main') options = { height: 352 }
 
-    const outDir = path.join(...outDirArr)
+    const outDir = join(...[imgPath, `${outputDir}@${res}`, img.key])
+    mkdirSync(outDir, {recursive: true})
 
-    function outputSharpImage(inputPath, options, outputPath, outputFormat) {
-      let img = sharp(inputPath).resize(options)
-
-      if (outputFormat == 'png') img.png()
-      else if (outputFormat == 'webp') img.webp()
-      else if (outputFormat == 'avif') img.avif({
-        quality: 70
-      })
-
-      img.toFile(outputPath, (err,) => {
-        if (err) console.log(err)
-      })
+    async function outputSharpImage(inputPath, options, outputPath, outputFormat) {
+      let conversionOptions = {}
+      if (outputFormat == 'avif') conversionOptions['quality'] = 70
+      await sharp(inputPath).resize(options)[outputFormat](conversionOptions).toFile(outputPath)
     }
 
+    let promises = []
     if (img.imageArr) for (const i of img.imageArr) {
-      const inputPath = path.join(dir, img.key, i)
-      const outputPath = path.join(outDir, path.basename(inputPath, '.png') + '.' + outputFormat)
+      const inputPath = join(dir, img.key, i)
+      const outputPath = join(outDir, basename(inputPath, '.png') + '.' + outputFormat)
 
-      outputSharpImage(inputPath, options, outputPath, outputFormat)
+      promises.push(outputSharpImage(inputPath, options, outputPath, outputFormat))
     } else {
-      const inputPath = path.join(dir, img.key + '.png')
-      const outputPath = path.join(outDir, '0.' + outputFormat)
+      const inputPath = join(dir, img.key + '.png')
+      const outputPath = join(outDir, '0.' + outputFormat)
 
-      outputSharpImage(inputPath, options, outputPath, outputFormat)
+      promises.push(outputSharpImage(inputPath, options, outputPath, outputFormat))
     }
+    await Promise.all(promises)
   } catch (err) {
     console.log(img, err)
     process.exit()
   }
 }
 
+let parentPromises = []
 for (const res of resizeArr) for (const imgType of ['png','webp','avif']) {
-  for (const img of deviceDirArr) createImg(img, res, deviceDirPath, imgType, 'device')
-  for (const img of lowResDirArr) createImg(img, res, lowResDirPath, imgType, 'device')
-  for (const img of imageDirArr)  createImg(img, res, imageDirPath,  imgType, 'images')
+  for (const img of deviceDirArr) parentPromises.push(createImg(img, res, deviceDirPath, imgType, 'device'))
+  for (const img of lowResDirArr) parentPromises.push(createImg(img, res, lowResDirPath, imgType, 'device'))
+  for (const img of imageDirArr)  parentPromises.push(createImg(img, res, imageDirPath,  imgType, 'images'))
 }
+await Promise.all(parentPromises)
