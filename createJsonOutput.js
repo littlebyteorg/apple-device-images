2e618548a2eadf530a18f2a207e6ac29cce85e45
Ryan Seaboyer <ryan.seaboyer@icloud.com>
2025-08-13 08:13:03 -0300

    Add support for dark mode logo images

--- createJsonOutput.js
+++ createJsonOutput.js
@@ -1,54 +1,61 @@
 import { readdirSync, existsSync, mkdirSync, writeFileSync, statSync } from 'fs'
 
-const keyArr = new Set(...[
-    [
-        ...readdirSync('device'),
-        ...readdirSync('device-lowres')
-    ].filter(x => x != '.DS_Store')
-    .map(x => x.replace(".png", "")) // Remove file extension
-])
+const createJson = function(folderName) {
 
-let retArr = []
+    const keyArr = new Set(...[
+        [
+            ...readdirSync(folderName),
+            ...(existsSync(`${folderName}-lowres`) ? readdirSync(`${folderName}-lowres`) : [])
+        ].filter(x => x != '.DS_Store')
+        .map(x => x.replace(".png", "")) // Remove file extension
+    ])
 
-for (const key of keyArr) {
-    let lowresExists = existsSync(`device-lowres/${key}.png`)
-    let pngExists = existsSync(`device/${key}.png`)
-    let folderExists = existsSync(`device/${key}/`) && statSync(`device/${key}/`).isDirectory()
-    
-    if (folderExists) {
-        const folderContents = readdirSync(`device/${key}`)
-        let images = []
+    let retArr = []
 
-        for (const folderItem of folderContents) {
-            if (folderItem.endsWith("_dark.png")) continue
-            if (!folderItem.endsWith(".png")) continue
-            const baseName = folderItem.replace(".png", "")
-            images.push({
-                id: baseName,
-                dark: folderContents.includes(`${baseName}_dark.png`)
+    for (const key of keyArr) {
+        if (key.endsWith("_dark")) continue
+        let lowresExists = existsSync(`${folderName}-lowres/${key}.png`)
+        let pngExists = existsSync(`${folderName}/${key}.png`)
+        let folderExists = existsSync(`${folderName}/${key}/`) && statSync(`${folderName}/${key}/`).isDirectory()
+        
+        if (folderExists) {
+            const folderContents = readdirSync(`${folderName}/${key}`)
+            let images = []
+
+            for (const folderItem of folderContents) {
+                if (folderItem.endsWith("_dark.png")) continue
+                if (!folderItem.endsWith(".png")) continue
+                const baseName = folderItem.replace(".png", "")
+                images.push({
+                    id: baseName,
+                    dark: folderContents.includes(`${baseName}_dark.png`)
+                })
+            }
+
+            retArr.push({
+                key: key,
+                count: images.length,
+                dark: images.filter(x => x.dark).length > 0,
+                index: images
             })
         }
 
-        retArr.push({
-            key: key,
-            count: images.length,
-            dark: images.filter(x => x.dark).length > 0,
-            index: images
-        })
+        else if (pngExists || lowresExists) {
+            retArr.push({
+                key: key,
+                count: 1,
+                dark: existsSync(`${folderName}/${key}_dark.png`) || existsSync(`${folderName}-lowres/${key}_dark.png`),
+                index: [{
+                    id: 0,
+                    dark: existsSync(`${folderName}/${key}_dark.png`) || existsSync(`${folderName}-lowres/${key}_dark.png`)
+                }]
+            })
+        }
     }
 
-    else if (pngExists || lowresExists) {
-        retArr.push({
-            key: key,
-            count: 1,
-            dark: false,
-            index: [{
-                id: 0,
-                dark: false
-            }]
-        })
-    }
+    return retArr;
 }
 
 if (!existsSync('out')) mkdirSync('out')
-writeFileSync('out/main.json', JSON.stringify(retArr))
+writeFileSync('out/main.json', JSON.stringify(createJson('device')))
+writeFileSync('out/logos.json', JSON.stringify(createJson('images')))
